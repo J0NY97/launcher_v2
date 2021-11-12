d1b5c0058cfb60c72e355cb5d6764c78de67d802
J0NY97 <jony.salmi97@gmail.com>
2021-11-12 15:19:25 +0200

    i forgot to add these

---
+++ launcher.h
@@ -0,0 +1,14 @@
+#ifndef LAUNCHER_H
+# define LAUNCHER_H
+
+#include "libft.h"
+#include "libpf.h"
+#include "libui.h"
+
+typedef struct s_launcher
+{
+	t_ui_layout		layout;
+	t_ui_window		*win_main;
+}					t_launcher;
+
+#endif
