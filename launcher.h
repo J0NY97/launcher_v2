c0fbbe13a0b9c3cfc01bd2d1c138f72b4f30f979
J0NY97 <jony.salmi97@gmail.com>
2021-11-12 18:10:58 +0200

    the correct menu will now show when you press the correlating button.

--- launcher.h
+++ launcher.h
@@ -8,7 +8,22 @@
 typedef struct s_launcher
 {
 	t_ui_layout		layout;
+
 	t_ui_window		*win_main;
+
+	t_ui_element	*main_menu;
+
+	t_ui_element	*play_button;
+	t_ui_element	*editor_button;
+	t_ui_element	*settings_button;
+	t_ui_element	*quit_button;
+	
+	t_list			*menu_buttons; // t_ui_element *
+	t_ui_element	*active_menu_button;
+
+	t_ui_element	*play_menu;
+	t_ui_element	*editor_menu;
+	t_ui_element	*settings_menu;
 }					t_launcher;
 
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,63 @@
 #include "launcher.h"
 
+void	play_events(t_launcher *launcher, SDL_Event e)
+{
+	
+}
+
+void	editor_events(t_launcher *launcher, SDL_Event e)
+{
+
+}
+
+void	settings_events(t_launcher *launcher, SDL_Event e)
+{
+
+}
+
+void	user_events(t_launcher *launcher, SDL_Event e)
+{
+	ui_list_radio_event(launcher->menu_buttons, &launcher->active_menu_button);
+	launcher->play_menu->show = launcher->active_menu_button == launcher->play_button;
+	launcher->editor_menu->show = launcher->active_menu_button == launcher->editor_button;
+	launcher->settings_menu->show = launcher->active_menu_button == launcher->settings_button;
+
+	if (launcher->play_menu->show)
+		play_events(launcher, e);
+	if (launcher->editor_menu->show)
+		editor_events(launcher, e);
+	if (launcher->settings_menu->show)
+		settings_events(launcher, e);
+}
+
 void	launcher_init(t_launcher *launcher)
 {
 	launcher->win_main = ui_list_get_window_by_id(launcher->layout.windows, "win_main");
+
+	// Main Menu
+	launcher->main_menu = ui_list_get_element_by_id(launcher->layout.elements, "main_menu");
+
+	// Play Menu
+	launcher->play_menu = ui_list_get_element_by_id(launcher->layout.elements, "play_menu");
+
+	// Editor Menu
+	launcher->editor_menu = ui_list_get_element_by_id(launcher->layout.elements, "editor_menu");
+
+	// Settings Menu
+	launcher->settings_menu = ui_list_get_element_by_id(launcher->layout.elements, "settings_menu");
+
+	// Buttons
+	launcher->play_button = ui_list_get_element_by_id(launcher->layout.elements, "play_button");
+	launcher->editor_button = ui_list_get_element_by_id(launcher->layout.elements, "editor_button");
+	launcher->settings_button = ui_list_get_element_by_id(launcher->layout.elements, "settings_button");
+
+	add_to_list(&launcher->menu_buttons, launcher->play_button, sizeof(t_ui_element));
+	add_to_list(&launcher->menu_buttons, launcher->editor_button, sizeof(t_ui_element));
+	add_to_list(&launcher->menu_buttons, launcher->settings_button, sizeof(t_ui_element));
+
+//	ui_label_font_set(ui_button_get_label_element(launcher->play_button), "fonts/AmazDoom.ttf");
+
+	launcher->quit_button = ui_list_get_element_by_id(launcher->layout.elements, "quit_button");
 }
 
 int	main(void)
@@ -18,12 +73,18 @@ int	main(void)
 	run = 1;
 	while (run)
 	{
-		if (launcher.win_main->wants_to_close)
+		if (launcher.win_main->wants_to_close
+			|| launcher.quit_button->state == UI_STATE_CLICK)
 			run = 0;
 		while (SDL_PollEvent(&e))
 		{
 			ui_layout_event(&launcher.layout, e);
+			user_events(&launcher, e);
+			if (e.key.keysym.scancode == SDL_SCANCODE_P)
+				ui_element_print(launcher.play_button);
 		}
+		// TODO: FIX: first element doesnt want to show anything if we dont fill the win texture with full color;
+		ui_texture_fill(launcher.win_main->renderer, launcher.win_main->texture, 0xff00ff00);
 		ui_layout_render(&launcher.layout);
 	}
 	return (0);
