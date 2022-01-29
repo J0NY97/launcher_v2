3eda82e335d24c6e79d4b4f32dadda288f32d16f
Epicurius <nneronin@student.hive.fi>
2022-01-29 14:08:05 +0200

    added absolute path

--- unzip.c
+++ unzip.c
@@ -6,7 +6,7 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 13:51:03 by nneronin          #+#    #+#             */
-/*   Updated: 2022/01/29 13:54:53 by nneronin         ###   ########.fr       */
+/*   Updated: 2022/01/29 14:07:11 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	unzip_resources(void)
 {
-	if (access("./game/resources", F_OK))
+	if (access(ROOT_PATH"./game/resources", F_OK))
 	{
 		if (!access("./resources.zip", F_OK))
-			system("unzip -q resources.zip -d ./game");
+			system("unzip -q resources.zip -d "ROOT_PATH"./game");
 		ft_printf("ZIP File not present.\n");
 		exit (1);
 	}
